22725736b0a1a52a0b20120ae8a115a9ae625f0a
Jared Hughes <j@redhugh.es>
2023-03-12 18:38:40 -0600

    Apply styling and content improvements to UI

--- app/lib/app/rankings.ex
+++ app/lib/app/rankings.ex
@@ -42,6 +42,40 @@ defmodule App.Rankings do
 
     (lsat_scores ++ gre_scores ++ gpa_scores)
     |> Enum.group_by(& &1.first_year_class.year)
-    |> Enum.sort_by(fn {key, _value} -> key end, :desc)
+    |> Enum.sort_by(fn {key, _} -> key end, :desc)
+    |> Enum.map(fn {y, scores} ->
+      {y, Enum.reduce(scores, %{}, &make_score_key/2)}
+    end)
+  end
+
+  @spec get_lsat_fact(integer() | charlist()) :: charlist()
+  def get_lsat_fact(school_id) do
+    most_recent =
+      LsatScore
+      |> join(:inner, [lsat], fyc in assoc(lsat, :first_year_class))
+      |> join(:inner, [lsat, _], s in assoc(lsat, :school))
+      |> where([lsat, _, _], lsat.school_id == ^school_id)
+      |> order_by([_, fyc, _], desc: fyc.year)
+      |> limit(1)
+      |> select([lsat, fyc, s], %{
+        year: fyc.year,
+        score: lsat.percentile_50,
+        school: s.name
+      })
+      |> Repo.one()
+
+    "In #{most_recent.year}, #{most_recent.school} had a 50th percentile LSAT score of #{most_recent.score}."
+  end
+
+  defp make_score_key(%LsatScore{} = exam, scores) do
+    Map.put(scores, :lsat, exam)
+  end
+
+  defp make_score_key(%GpaScore{} = exam, scores) do
+    Map.put(scores, :gpa, exam)
+  end
+
+  defp make_score_key(%GreScore{} = exam, scores) do
+    Map.put(scores, :gre, exam)
   end
 end
--- app/lib/app_web/live/ranking_live/index.ex
+++ app/lib/app_web/live/ranking_live/index.ex
@@ -26,7 +26,26 @@ defmodule AppWeb.RankingLive.Index do
     {:noreply,
      assign(socket, %{
        school: Rankings.get_school(school_id),
-       scores: Rankings.get_scores(school_id)
+       scores: Rankings.get_scores(school_id),
+       fact: Rankings.get_lsat_fact(school_id)
      })}
+  end
+
+  def score_cell(assigns) do
+    bg_class =
+      case assigns.percentile do
+        25 -> "bg-sky-400"
+        50 -> "bg-sky-500"
+        75 -> "bg-sky-600"
+      end
+
+    ~H"""
+    <div class={"#{bg_class} text-center rounded-lg text-white"}>
+      <div class="bg-slate-700 rounded-t-lg p-2 text-white uppercase text-sm font-semibold">
+        <%= @percentile %>th Percentile
+      </div>
+      <div class="text-[30px] p-5 font-bold"><%= @data %></div>
+    </div>
+    """
   end
 end
